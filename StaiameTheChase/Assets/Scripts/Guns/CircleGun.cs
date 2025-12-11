971535fc32c3cd72516d2bbe1c026772a30213c6
Guilherme-souzae <guilhermesouzae.contato@gmail.com>
2025-12-11 12:58:15 -0300

    feature: espirais!

--- StaiameTheChase/Assets/Scripts/Guns/CircleGun.cs
+++ StaiameTheChase/Assets/Scripts/Guns/CircleGun.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class CircleGun : BaseGun
 {
     public int bulletDensity = 10;
-    private const float spreadAngle = 360f;
     public override void shoot()
     {
+        float step = 360f / bulletDensity;
+
         for (int i = 0; i < bulletDensity; i++)
         {
-            float angle = -spreadAngle / 2 + (spreadAngle / (bulletDensity - 1)) * i;
+            float angle = step * i;
             Quaternion rotation = Quaternion.Euler(0, 0, transform.eulerAngles.z + angle);
             Instantiate(bulletPrefab, transform.position, rotation);
         }
---
+++ StaiameTheChase/Assets/Scripts/Guns/SpiralGun.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class SpiralGun : BaseGun
+{
+    public float angleStep = 10f;
+    public float delay = 0.05f;
+    public int shots = 50;
+
+    private bool isShooting = false;
+
+    public override void shoot()
+    {
+        if (!isShooting)
+            StartCoroutine(SpiralRoutine());
+    }
+
+    private IEnumerator SpiralRoutine()
+    {
+        isShooting = true;
+
+        float angle = 0f;
+
+        for (int i = 0; i < shots; i++)
+        {
+            Quaternion rotation = Quaternion.Euler(0, 0, transform.eulerAngles.z + angle);
+            Instantiate(bulletPrefab, transform.position, rotation);
+
+            angle += angleStep;
+
+            yield return new WaitForSeconds(delay);
+        }
+
+        isShooting = false;
+    }
+}
